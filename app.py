c4eb2c6f97fbba8493ad6545ab6cf6a9d9f98c9c
Mack Oyler <120589371+MackOyler@users.noreply.github.com>
2024-05-17 14:13:36 -0500

    setting up api

--- app.py
+++ app.py
@@ -6,7 +6,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello world! updated"
+    return render_template("index.html")
+
+def fetch_spacex_launches():
+    url = "https://api.spacexdata.com/v4/launches"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return []
+    
+launches = fetch_spacex_launches()
+print(launches[0])
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True)  
